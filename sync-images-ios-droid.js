12270f4481e67c04768bacda0275f470d3131a00
erabbit <rabbitom@gmail.com>
2018-01-08 18:00:30 +0800

    将Android图片添加到Xamarin工程

--- sync-images-ios-droid.js
+++ sync-images-ios-droid.js
@@ -3,11 +3,17 @@
  */
 var iOSImages = require('./lib/ios-images.js');
 var AndroidImages = require('./lib/android-images.js');
+var ProjectItems = require('./lib/xamarin-project-items.js');
 
 var src = new iOSImages();
-src.loadFromPath('/Users/Tom/Downloads/耳机/UI_重新切图');
+src.loadFromPath('/Users/Tom/Downloads/耳机/UI_重新切图/_buttons');
 
 var dest = new AndroidImages();
 dest.images = src.images;
 dest.saveToProject('/Users/Tom/Desktop/Projects/EarPhone/EarPhone/Droid');
 console.log(`${dest.imageCount} images, ${dest.fileCount} files saved.`);
+
+var project = new ProjectItems('/Users/Tom/Desktop/Projects/EarPhone/EarPhone/Droid');
+for (var file of dest.newFiles)
+    project.addFileItem(file, 'AndroidResource');
+project.saveToProject();
