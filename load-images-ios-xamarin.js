6334f3f0d056a936bc71721e2c81407c584b97ff
erabbit <rabbitom@gmail.com>
2018-08-03 12:02:29 +0800

    支持覆盖同名图片

--- load-images-ios-xamarin.js
+++ load-images-ios-xamarin.js
@@ -5,26 +5,29 @@ var iOSImages = require('./lib/ios-images.js');
 var AndroidImages = require('./lib/android-images.js');
 var ProjectItems = require('./lib/xamarin-project-items.js');
 
-var imagesPath = '/Users/ziv/Downloads/temp';
-var xamarinSolutionPath = '/Users/ziv/Desktop/EarPhone';
+var imagesPath = '/Users/Tom/Downloads/EarPhone';
+var xamarinSolutionPath = '/Users/Tom/Desktop/Projects/EarPhone/EarPhone';
 
 var src = new iOSImages();
 src.loadFromPath(imagesPath);
 
 src.saveToProject(xamarinSolutionPath + '/iOS');
-var iosProject = new ProjectItems(xamarinSolutionPath + '/iOS');
-for (var folder of src.newFolders)
-    iosProject.addFileItem(folder, 'Folder');
-for (var file of src.newFiles)
-    iosProject.addFileItem(file, 'ImageAsset');
-iosProject.saveToProject();
+if((src.newFiles.length > 0) || (src.newFolders.length > 0)) {
+    var iosProject = new ProjectItems(xamarinSolutionPath + '/iOS');
+    for (var folder of src.newFolders)
+        iosProject.addFileItem(folder, 'Folder');
+    for (var file of src.newFiles)
+        iosProject.addFileItem(file, 'ImageAsset');
+    iosProject.saveToProject();    
+}
 
 var dest = new AndroidImages();
 dest.images = src.images;
 dest.saveToProject(xamarinSolutionPath + '/Droid');
 console.log(`${dest.imageCount} images, ${dest.fileCount} files saved.`);
-
-var droidProject = new ProjectItems(xamarinSolutionPath + '/Droid');
-for (var file of dest.newFiles)
-    droidProject.addFileItem(file, 'AndroidResource');
-droidProject.saveToProject();
+if(dest.newFiles.length > 0) {
+    var droidProject = new ProjectItems(xamarinSolutionPath + '/Droid');
+    for (var file of dest.newFiles)
+        droidProject.addFileItem(file, 'AndroidResource');
+    droidProject.saveToProject();
+}
